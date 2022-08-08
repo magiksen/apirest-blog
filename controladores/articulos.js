5ffd17e91e82d9aa25ce55e836c54f7544c6d960
magiksen <elemejore@gmail.com>
2022-08-08 13:55:14 -0400

    Crear helper de validacion

--- controladores/articulos.js
+++ controladores/articulos.js
@@ -1,4 +1,4 @@
-const validator = require("validator");
+const { validarArticulo } = require("../helpers/validar")
 const Articulo = require("../modelos/Articulo");
 
 const prueba = (req, res) => {
@@ -33,13 +33,7 @@ const crear = (req, res) => {
 
 	// Validar
 	try{
-		let validar_titulo = !validator.isEmpty(parametros.titulo) &&
-								validator.isLength(parametros.titulo, {min: 5, max: undefined});
-		let validar_contenido = !validator.isEmpty(parametros.contenido);
-
-		if (!validar_titulo || !validar_contenido) {
-			throw new Error("No se ha validado la informacion !!");
-		}
+		validarArticulo(parametros);
 
 	}catch(error){
 		return res.status(400).json({
@@ -155,13 +149,7 @@ const editar = (req, res) => {
 
 	// validar datos
 	try{
-		let validar_titulo = !validator.isEmpty(parametros.titulo) &&
-			validator.isLength(parametros.titulo, {min: 5, max: undefined});
-		let validar_contenido = !validator.isEmpty(parametros.contenido);
-
-		if (!validar_titulo || !validar_contenido) {
-			throw new Error("No se ha validado la informacion !!");
-		}
+		validarArticulo(parametros);
 
 	}catch(error){
 		return res.status(400).json({
